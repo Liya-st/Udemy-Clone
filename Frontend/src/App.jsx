1c7ef264c8b9f1e2b77d036e872bb24c4abf0b07
Maedot Amha <maedotamha@gmail.com>
2024-05-13 00:02:27 +0300

    course

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,16 +5,20 @@ import Nav from './Components/Nav'
 import '@mantine/core/styles.css';
 import { Routes,Route } from "react-router-dom";
 import Cart from "./Components/Cart";
+import { Courses } from "./Pages/Instructor/Courses.jsx";
 
 function App() {
+  const isNavbarVisible = ['/Cart', '/Login', '/SignUp','/'];
 
   return (
     <>
-    <Nav />
+    {isNavbarVisible.includes(window.location.pathname) && <Nav />}
     <Routes>
+       <Route path="Pages/Instructor/Courses" element={<Courses />} />
        <Route path="/Components/Cart" element={<Cart />} />
-             <Route path="/Pages/Signup" element={<SignUp />} />
+       <Route path="/Pages/Signup" element={<SignUp />} />
       <Route path="/Pages/Login" element={<Login />} />
+      
     </Routes>
     <Footer/>
     </>
