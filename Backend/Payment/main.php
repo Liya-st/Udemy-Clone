3ab73f5b57168d1f7f218b49926692282478acd9
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2024-06-14 02:01:45 +0300

    Add files via upload

---
+++ Backend/Payment/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Payment</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <h1>Payment</h1>
+  <form action="checkout.php" method="post">
+    <button type="submit" name="checkout">Proceed to Payment</button>
+  </form>
+</body>
+</html>
